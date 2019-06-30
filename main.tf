b161185fa259df5d8812834bfe8cf7efe0877280
1adam <1adam@users.noreply.github.com>
2019-06-30 21:07:09 +0000

    upd to terraform 0.12

--- main.tf
+++ main.tf
@@ -3,15 +3,16 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 data "aws_ami" "img" {
-    owners = ["099720109477"]
-    most_recent = true
-    filter {
-        name = "manifest-location"
-        values = ["099720109477/ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-    }
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "manifest-location"
+    values = ["099720109477/ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
 }
 
 data "aws_availability_zones" "available" {
@@ -19,172 +20,173 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "${var.vpc_cidr}"
-    enable_dns_support = true
-    enable_dns_hostnames = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-    tags = {
-        Name = "vpc01"
-        Project = "DevOpsASGtest"
-    }
+  tags = {
+    Name    = "vpc01"
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_subnet" "pub1" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "${var.pub_subnet_cidr_1}"
-    map_public_ip_on_launch = true
-    availability_zone = "${data.aws_availability_zones.available.names[0]}"
-    tags = {
-        Name = "subpub01"
-        Project = "DevOpsASGtest"
-    }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.pub_subnet_cidr_1
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name    = "subpub01"
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_subnet" "pub2" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "${var.pub_subnet_cidr_2}"
-    map_public_ip_on_launch = true
-    availability_zone = "${data.aws_availability_zones.available.names[1]}"
-    tags = {
-        Name = "subpub02"
-        Project = "DevOpsASGtest"
-    }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.pub_subnet_cidr_2
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name    = "subpub02"
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_internet_gateway" "igw01" {
-    vpc_id = "${aws_vpc.main.id}"
-    tags = {
-        Project = "DevOpsASGtest"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_security_group" "primary" {
-    name_prefix = "allow-ssh-sg-"
-    description = "Allow inbound SSH"
-    vpc_id = "${aws_vpc.main.id}"
+  name_prefix = "allow-ssh-sg-"
+  description = "Allow inbound SSH"
+  vpc_id      = aws_vpc.main.id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Project = "DevOpsASGtest"
-    }
+  tags = {
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_route_table" "main_rt" {
-    vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.igw01.id}"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw01.id
+  }
 
-    tags = {
-        Name = "devops-asg-tst-main-rt"
-        Project = "DevOpsASGtest"
-    }
+  tags = {
+    Name    = "devops-asg-tst-main-rt"
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_route_table_association" "pub1_rta" {
-    subnet_id = "${aws_subnet.pub1.id}"
-    route_table_id = "${aws_route_table.main_rt.id}"
+  subnet_id      = aws_subnet.pub1.id
+  route_table_id = aws_route_table.main_rt.id
 }
 
 resource "aws_route_table_association" "pub2_rta" {
-    subnet_id = "${aws_subnet.pub2.id}"
-    route_table_id = "${aws_route_table.main_rt.id}"
+  subnet_id      = aws_subnet.pub2.id
+  route_table_id = aws_route_table.main_rt.id
 }
 
 resource "aws_key_pair" "pri_key" {
-    key_name_prefix = "devops-asg-tst-key-"
-    public_key = "${var.ssh_pubkey}"
+  key_name_prefix = "devops-asg-tst-key-"
+  public_key      = var.ssh_pubkey
 }
 
 resource "aws_launch_template" "main_lt" {
-    name_prefix = "devops-asg-tst-"
-    description = "for testing porpoises only"
-    block_device_mappings {
-        device_name = "/dev/sda1"
-        ebs {
-            volume_size = 10
-            volume_type = "gp2"
-        }
+  name_prefix = "devops-asg-tst-"
+  description = "for testing porpoises only"
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size = 10
+      volume_type = "gp2"
     }
-    key_name = "${aws_key_pair.pri_key.key_name}"
-    image_id = "${data.aws_ami.img.id}"
-    instance_type = "${var.inst_type}"
-    vpc_security_group_ids = ["${aws_security_group.primary.id}"]
-    tag_specifications {
-        resource_type = "instance"
-        tags = {
-            Name = "devops-asg-tst"
-            Project = "DevOpsASGtest"
-        }
-    }
+  }
+  key_name               = aws_key_pair.pri_key.key_name
+  image_id               = data.aws_ami.img.id
+  instance_type          = var.inst_type
+  vpc_security_group_ids = [aws_security_group.primary.id]
+  tag_specifications {
+    resource_type = "instance"
     tags = {
-        Project = "DevOpsASGtest"
+      Name    = "devops-asg-tst"
+      Project = "DevOpsASGtest"
     }
+  }
+  tags = {
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_autoscaling_group" "devops_asg_grp" {
-    name_prefix = "devops-asg-tst-"
-    max_size = 1
-    min_size = 1
-    desired_capacity = 1
-    target_group_arns = ["${aws_lb_target_group.pri_tg.arn}"]
-    vpc_zone_identifier = ["${aws_subnet.pub1.id}", "${aws_subnet.pub2.id}"]
-    launch_template {
-        id = "${aws_launch_template.main_lt.id}"
-        version = "$Latest"
-    }
-    tag {
-        key = "Name"
-        value = "devops-asg-test-inst"
-        propagate_at_launch = true
-    }
-    tag {
-        key = "Project"
-        value = "DevOpsASGtest"
-        propagate_at_launch = true
-    }
+  name_prefix         = "devops-asg-tst-"
+  max_size            = 1
+  min_size            = 1
+  desired_capacity    = 1
+  target_group_arns   = [aws_lb_target_group.pri_tg.arn]
+  vpc_zone_identifier = [aws_subnet.pub1.id, aws_subnet.pub2.id]
+  launch_template {
+    id      = aws_launch_template.main_lt.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = "devops-asg-test-inst"
+    propagate_at_launch = true
+  }
+  tag {
+    key                 = "Project"
+    value               = "DevOpsASGtest"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_lb" "main_lb" {
-    name_prefix = "asgtst"
-    internal = false
-    load_balancer_type = "network"
-    subnets = ["${aws_subnet.pub1.id}","${aws_subnet.pub2.id}"]
-    enable_cross_zone_load_balancing = true
+  name_prefix                      = "asgtst"
+  internal                         = false
+  load_balancer_type               = "network"
+  subnets                          = [aws_subnet.pub1.id, aws_subnet.pub2.id]
+  enable_cross_zone_load_balancing = true
 
-    tags = {
-        Project = "DevOpsASGtest"
-    }
+  tags = {
+    Project = "DevOpsASGtest"
+  }
 }
 
 resource "aws_lb_target_group" "pri_tg" {
-    name_prefix = "asgtst"
-    port = 22
-    protocol = "TCP"
-    vpc_id = "${aws_vpc.main.id}"
+  name_prefix = "asgtst"
+  port        = 22
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.main.id
 }
 
 resource "aws_lb_listener" "main_lb_listen" {
-    load_balancer_arn = "${aws_lb.main_lb.arn}"
-    port = 22
-    protocol = "TCP"
-    default_action {
-        type = "forward"
-        target_group_arn = "${aws_lb_target_group.pri_tg.arn}"
-    }
+  load_balancer_arn = aws_lb.main_lb.arn
+  port              = 22
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.pri_tg.arn
+  }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,23 +1,25 @@
 variable "vpc_cidr" {
-    type = "string"
-    default = "10.20.30.0/24"
+  type    = string
+  default = "10.20.30.0/24"
 }
 
 variable "pub_subnet_cidr_1" {
-    type = "string"
-    default = "10.20.30.0/25"
+  type    = string
+  default = "10.20.30.0/25"
 }
+
 variable "pub_subnet_cidr_2" {
-    type = "string"
-    default = "10.20.30.128/25"
+  type    = string
+  default = "10.20.30.128/25"
 }
 
 variable "inst_type" {
-    type = "string"
-    default = "t3.nano"
+  type    = string
+  default = "t3.nano"
 }
 
 variable "ssh_pubkey" {
-    type = "string"
-    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk5Zbz9NWgmkT2SZI+2U14vq21xDkyTndK79j11yoov7TfIRagLr0iKM8FAN2yrLg51vxDKH/Nt+TJxoQUwRGAYzxA1Tc/u8h92L3011MsADLwkGKemj8XD2xx8TB60yEF0SLTs/d+tyQ71eRkXcaYOEjbXBEL0/crZ41oAKX7Z3zjArpHMqpu5qVmMWVVz41zp9IYalIGsjq3Rdu/J2YoA4g8/iRk9E41sMhoUjT1O3aLvUD/RuDqgBRL0l7A5Xbz9Ye/uOdy2Cu2pfjkikqjDLLuOITOxh/F0+2IjyZROqcw5rrZ/VAj2V3quP07eg/65cRiiwRgsxOOxbKPJ1lZ laboratory"
+  type    = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk5Zbz9NWgmkT2SZI+2U14vq21xDkyTndK79j11yoov7TfIRagLr0iKM8FAN2yrLg51vxDKH/Nt+TJxoQUwRGAYzxA1Tc/u8h92L3011MsADLwkGKemj8XD2xx8TB60yEF0SLTs/d+tyQ71eRkXcaYOEjbXBEL0/crZ41oAKX7Z3zjArpHMqpu5qVmMWVVz41zp9IYalIGsjq3Rdu/J2YoA4g8/iRk9E41sMhoUjT1O3aLvUD/RuDqgBRL0l7A5Xbz9Ye/uOdy2Cu2pfjkikqjDLLuOITOxh/F0+2IjyZROqcw5rrZ/VAj2V3quP07eg/65cRiiwRgsxOOxbKPJ1lZ laboratory"
 }
+
